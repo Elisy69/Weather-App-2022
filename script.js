2c712f03a7bbe1ee0f1e87b3a66db1b0ba0c6119
Elisei <strashevsky.elisey@gmail.com>
2023-01-17 02:29:37 +0300

    first two tabs

---
+++ script.js
@@ -0,0 +1,15 @@
+const tabs = document.querySelectorAll(".tab");
+const selectedWindow = document.querySelectorAll(".weatherWindows");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((tab) => tab.classList.remove("pressed"));
+    tab.classList.add("pressed");
+    const selected = document.querySelector(tab.dataset.option);
+    console.log(selected);
+    selectedWindow.forEach((window) => window.classList.remove("active"));
+    selected.classList.add("active");
+  });
+});
+
+console.log(tabs);
